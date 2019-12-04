ca64416e89c83e9464a5b55a1967e2d5aaf2a9ad
mootechs <jodquach@ucdavis.edu>
2019-12-04 13:27:16 -0800

    Fixed filter by location

--- Down?/Down?/ApiEvent.swift
+++ Down?/Down?/ApiEvent.swift
@@ -143,7 +143,6 @@ public struct Event {
 
     */
     init(dict: [String: Any], autoID: String) {
-        print(dict)
         let startTimeStamp = dict["startTime"] as? Timestamp
         let endTimeStamp = dict["endTime"] as? Timestamp
         let geoPoint = dict["location"] as? GeoPoint
@@ -167,6 +166,43 @@ public struct Event {
 public class ApiEvent {
     private static let db = Firestore.firestore()
     
+    //https://www.geodatasource.com/developers/swift
+    ///  This function converts decimal degrees to radians
+    private static func deg2rad(_ deg:Double) -> Double {
+        return deg * Double.pi / 180
+    }
+
+    ///  This function converts radians to decimal degrees
+    private static func rad2deg(_ rad:Double) -> Double {
+        return rad * 180.0 / Double.pi
+    }
+
+    ///  This function calculates the distance between two corrdinates in miles
+    private static func distanceInMiles(lat1:Double, lon1:Double, lat2:Double, lon2:Double) -> Double {
+        let theta = lon1 - lon2
+        var dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta))
+        dist = acos(dist)
+        dist = rad2deg(dist)
+        dist = dist * 60 * 1.1515
+
+        return dist
+    }
+    
+    /// Filters events by a given distance and returns the filtered events
+    private static func filterByDistance(events: [Event], currentLocation: EventLocation, distance: Double) -> [Event] {
+        var filtered = [Event]()
+        for event in events {
+            if let lat = event.location?.latitude, let lon = event.location?.longitude {
+                let distFromCurrLocation = distanceInMiles(lat1: currentLocation.latitude,
+                                                           lon1: currentLocation.longitude, lat2: lat, lon2: lon)
+                if distFromCurrLocation <= distance {
+                    print("curr: \(distFromCurrLocation) dist:\(distance)")
+                    filtered.append(event)
+                }
+            }
+        }
+        return filtered
+    }
     /**
      Fetches a list of event IDs that the user is down for.
 
@@ -340,27 +376,8 @@ public class ApiEvent {
        */
     public static func getUnviewedEventFilter(uid: String, categories: [String], distance: Double?, currentLocation: EventLocation?, completion: @escaping ([Event]) -> Void) {
         getViewedEventIDs(uid: uid) { viewedEventIDs in
-            var ref = db.collection("events")
+            let ref = db.collection("events")
                 .whereField("endTime", isGreaterThanOrEqualTo: Timestamp(date: Date()))
-            
-            if let distance = distance, let currentLocation = currentLocation {
-                // ~1 mile of lat and lon in degrees
-                let lat = 0.0144927536231884
-                let lon = 0.0181818181818182
-
-                let lowerLat = currentLocation.latitude - (lat * distance)
-                let lowerLon = currentLocation.longitude - (lon * distance)
-
-                let greaterLat = currentLocation.latitude + (lat * distance)
-                let greaterLon = currentLocation.longitude + (lon * distance)
-
-                let lesserGeopoint = GeoPoint(latitude: lowerLat, longitude: lowerLon)
-                let greaterGeopoint = GeoPoint(latitude: greaterLat, longitude: greaterLon)
-                
-                ref = ref.whereField("location", isGreaterThan: lesserGeopoint)
-                        .whereField("location", isLessThan: greaterGeopoint)
-            }
-            
             ref.getDocuments() { (snapshot, error) in
                 var unviewedEvents = [Event]()
                 if error != nil { return }
@@ -377,6 +394,9 @@ public class ApiEvent {
                             unviewedEvents.append(event)
                         }
                     }
+                }
+                if let distance = distance, let currentLocation = currentLocation {
+                    unviewedEvents = self.filterByDistance(events: unviewedEvents, currentLocation: currentLocation, distance: distance)
                 }
                 completion(unviewedEvents)
             }
@@ -607,6 +627,7 @@ public class ApiEvent {
             }
         }
     }
+
 }
 
 
